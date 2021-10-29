7296ff98e3a4378d6eb4ed9f8b09c3350d530c07
Vinicius Prado <vinicavagna@gmail.com>
2021-10-29 13:51:04 -0300

    :hammer: minor refactor at deploy-commands

--- src/utils/deploy-commands.ts
+++ src/utils/deploy-commands.ts
@@ -4,19 +4,21 @@ dotenv.config();
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
 import fs from "fs-extra";
+import path from "path";
 
 const clientId = process.env.CLIENT_ID;
 const authToken = process.env.AUTH_TOKEN;
+const pathCommands = path.join(__dirname, "..", "commands");
 
 export const deployCommands = async (guildId: string) => {
   const rest = new REST({ version: "9" }).setToken(authToken);
 
   const commands: any[] = [];
 
-  const cmdFiles = fs.readdirSync("src/commands/");
+  const cmdFiles = fs.readdirSync(pathCommands);
 
   for (const file of cmdFiles) {
-    const command = require(`../commands/${file}`).default;
+    const command = require(path.join(pathCommands, file)).default;
     commands.push(command.data.toJSON());
   }
 
